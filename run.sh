cee1f608ecdd9bd3ecbfd39ff916666c40656cc0
Teh Kok How <funcoolgeek@gmail.com>
2019-03-28 13:32:30 +0800

    Print node role

--- run.sh
+++ run.sh
@@ -2,3 +2,9 @@
 set -x
 yes yes | redis-cli --cluster create --cluster-replicas 1 $(kubectl get pods -l app=redis-cluster -o jsonpath='{range.items[*]}{.status.podIP}:6379 ')
 redis-cli -h svc-redis-cluster cluster info
+nodes=`kubectl get pods -l app=redis-cluster | grep redis-cluster | wc -l`
+echo $nodes nodes
+for x in $(seq 0 $(( --nodes )));
+do 
+  kubectl exec redis-cluster-$x -- redis-cli role; 
+done
