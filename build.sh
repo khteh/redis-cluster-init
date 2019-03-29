87c42616063a2430f4df499ee37148412242c617
Teh Kok How <funcoolgeek@gmail.com>
2019-03-29 15:20:55 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 $(aws ecr get-login --no-include-email)
 docker build -t redis-cluster-init .
-docker tag redis-cluster-init:latest 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/redis-cluster-init:latest
-docker push 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/redis-cluster-init:latest
+#docker tag redis-cluster-init:latest 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/redis-cluster-init:latest
+#docker push 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/redis-cluster-init:latest
 docker tag redis-cluster-init:latest khteh/redis-cluster-init:latest
 docker push khteh/redis-cluster-init:latest
