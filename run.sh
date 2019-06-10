f6155bf6bf7d47b76ba1d96d19c59a18671c080e
Teh Kok How <khteh@KHTeh-T490.biz4x.com>
2019-06-10 09:36:54 +0800

    Remove fix-ip.sh which is not needed

--- run.sh
+++ run.sh
@@ -6,6 +6,5 @@ nodes=`kubectl get pods -l app=redis-cluster | grep redis-cluster | wc -l`
 echo $nodes nodes
 for x in $(seq 0 $(( --nodes )));
 do 
-  kubectl exec redis-cluster-$x -- /conf/fix-ip.sh;
   kubectl exec redis-cluster-$x -- redis-cli role; 
 done
