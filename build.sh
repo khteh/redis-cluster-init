00d0fe4326c73d3781c691f8021f683a7a4d51fa
Kok How, Teh <funcoolgeek@gmail.com>
2021-11-23 11:18:10 +0800

    Upgrade k8s debian sources to strectch

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$(aws ecr get-login --no-include-email)
+#$(aws ecr get-login --no-include-email)
 docker build -t redis-cluster-init .
 #docker tag redis-cluster-init:latest 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/redis-cluster-init:latest
 #docker push 701969852130.dkr.ecr.ap-southeast-1.amazonaws.com/redis-cluster-init:latest
