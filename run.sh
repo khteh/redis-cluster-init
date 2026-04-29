57e6c4989c63bcf3ba32ff073293474960270a91
Kok How, Teh <funcoolgeek@gmail.com>
2026-04-29 11:16:23 +0800

    Fix josnpath syntax of getting pod IPs and append them with port number

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-yes yes | redis-cli --cluster create --cluster-replicas 1 $(kubectl get pods -l app=redis-cluster -o jsonpath='{.items[*].status.podIP}:6379')
+yes yes | redis-cli --cluster create --cluster-replicas 1 $(kubectl get pods -l app=redis-cluster -o jsonpath='{range .items[*]}{.status.podIP}:6379 {end}')
 redis-cli -h svc-redis-cluster cluster info
 nodes=`kubectl get pods -l app=redis-cluster | grep redis-cluster | wc -l`
 echo $nodes nodes
